3c9a09c4a1d9255539b8d764740270db7de5982e
estevansta <cruz@therrb.co>
2019-04-15 19:41:33 -0500

    npm packages ignored, axios and cheerio on console

---
+++ server.js
@@ -0,0 +1,10 @@
+// Parses our HTML and helps us find elements. code skeleton via 18.2-05-scraping assignment
+var cheerio = require("cheerio");
+// Makes HTTP request for HTML page
+var axios = require("axios");
+
+// console.logging what the server.js is doing and a message to show that its connected
+console.log("\n***********************************\n" +
+            "Testing an cheerio and axios console.log\n" +
+            "\n***********************************\n");
+
